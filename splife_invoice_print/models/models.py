04637918a4e49f4dc1a64bb61fcbf38af99abcec
Loyal IT Solutions <13286560+loyalcomps@users.noreply.github.com>
2020-09-03 13:06:39 +0530

    invoice print

--- splife_invoice_print/models/models.py
+++ splife_invoice_print/models/models.py
@@ -10,3 +10,6 @@ class Hremployee(models.Model):
     _inherit = 'hr.employee'
 
     acc_number = fields.Char(string="A/C No.")
+    ifsc_code = fields.Char(string="IFSC Code")
+    bank_name = fields.Many2one('res.bank',string="Bank Name")
+    branch_name = fields.Char(string="Branch")
