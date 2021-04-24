c508e4742c4d182c8b9acccdebdc1a8120400bb6
Vishnu Ramesh <55851192+VISHNUVR-LOYALITSOLUTIONS@users.noreply.github.com>
2021-04-24 16:34:24 +0530

    Add files via upload

--- splife_gst_report/models/account_gst.py
+++ splife_gst_report/models/account_gst.py
@@ -24,7 +24,8 @@ class AccountMove(models.Model):
 
 
     @api.onchange('invoice_line_ids')
-    def _onchange_invoice_line_ids(self):
+    def account_gst_onchange_invoice_line_ids(self):
+        mon1 = []
 
         for i in self:
             if i.id:
@@ -34,7 +35,7 @@ class AccountMove(models.Model):
 
          # Generate one tax line per tax, however many invoice lines it's applied to
 
-        taxes_grouped = self.generate_tax()
+        taxes_grouped = self.account_generate_tax()
         gst_tax_lines = self.account_gst_tax_ids.browse([])
         gst_val = {}
 
@@ -52,14 +53,20 @@ class AccountMove(models.Model):
                 'reg_kfc_tax': inv_tax_lines[8] if inv_tax_lines[8] else 0,
 
             }
-            gst_tax_lines += gst_tax_lines.create(so_order)
+            mon3 = (0, 0, so_order)
+            mon1.append(mon3)
+        self.update({
 
-        self.account_gst_tax_ids = gst_tax_lines
+            'account_gst_tax_ids': mon1
+        })
+        #     gst_tax_lines += gst_tax_lines.create(so_order)
+        #
+        # self.account_gst_tax_ids = gst_tax_lines
         s = 1
         return
 
     # @api.onchange('line_ids')
-    def generate_tax(self):
+    def account_generate_tax(self):
         # self.gst_tax_ids.unlink()
 
         grouped_tax_lines = {}
@@ -67,6 +74,7 @@ class AccountMove(models.Model):
         duplicate_line_tax = {}
 
         for invoice_line in self.invoice_line_ids:
+            move_id = self.id
             if invoice_line.discount:
                 price = invoice_line.price_unit * (1 - (invoice_line.discount or 0.0) / 100.0)
             else:
@@ -135,9 +143,10 @@ class AccountMove(models.Model):
                 grouped_tax_lines[gst_account_id][8] += gstDict['re_kfc']
                 grouped_tax_lines[gst_account_id][9] += gstDict['re_csamt']
                 grouped_tax_lines[gst_account_id][10] += gstDict['re_iamt']
+                grouped_tax_lines[gst_account_id][11] = move_id
 
             else:
-                grouped_tax_lines[gst_account_id] = [0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0]
+                grouped_tax_lines[gst_account_id] = [0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0,0]
                 grouped_tax_lines[gst_account_id][0] = gst_account_id
                 grouped_tax_lines[gst_account_id][1] += gstDict['ka_cgst']
                 grouped_tax_lines[gst_account_id][2] += gstDict['ka_sgst']
@@ -150,6 +159,7 @@ class AccountMove(models.Model):
                 grouped_tax_lines[gst_account_id][8] += gstDict['re_kfc']
                 grouped_tax_lines[gst_account_id][9] += gstDict['re_csamt']
                 grouped_tax_lines[gst_account_id][10] += gstDict['re_iamt']
+                grouped_tax_lines[gst_account_id][11] = move_id
 
 
 
@@ -161,22 +171,27 @@ class AccountMove(models.Model):
     account_gst_tax_ids = fields.One2many("account.gst.tax", "move_id", string="Tax",)
 
 
-    def compute_taxes(self):
+    def account_compute_taxes(self):
         """Function used in other module to compute the taxes on a fresh invoice created (onchanges did not applied)"""
         account_invoice_tax = self.env['account.gst.tax']
         ctx = dict(self._context)
         for invoice in self:
             # Delete non-manual tax lines
-            self._cr.execute("DELETE FROM account_gst_tax WHERE move_id=%s", (invoice.id,))
-            # if self._cr.rowcount:
-            self.invalidate_cache()
+            if self.type not in ['out_refund','in_refund', 'out_receipt', 'in_receipt']:
 
-            # Generate one tax line per tax, however many invoice lines it's applied to
-            tax_grouped = invoice.generate_tax()
+                self._cr.execute("DELETE FROM account_gst_tax WHERE move_id=%s", (invoice.id,))
+                # if self._cr.rowcount:
+                self.invalidate_cache()
 
-            # Create new tax lines
-            for tax in tax_grouped.values():
-                account_invoice_tax.create(tax)
+                # Generate one tax line per tax, however many invoice lines it's applied to
+                tax_grouped = invoice.account_generate_tax()
+
+                # Create new tax lines
+                # if self.type not in ['out_invoice', 'out_refund', 'in_invoice', 'in_refund', 'out_receipt', 'in_receipt']:
+                for tax in tax_grouped.values():
+                    account_invoice_tax.create(tax)
+            else:
+                self.account_gst_onchange_invoice_line_ids()
 
         # dummy write on self to trigger recomputations
         return
@@ -187,7 +202,16 @@ class AccountMove(models.Model):
         request=super(AccountMove, self).create(vals)
 
         if any(line.tax_ids for line in request.invoice_line_ids) and not request.account_gst_tax_ids:
-            request.compute_taxes()
+            request.account_compute_taxes()
+        return request
+
+    @api.model
+    def write(self, vals):
+        request = super(AccountMove, self).write(vals)
+
+        for taxes in self:
+            if any(line.tax_ids for line in taxes.invoice_line_ids) and not taxes.account_gst_tax_ids:
+                taxes.account_compute_taxes()
         return request
 
 
